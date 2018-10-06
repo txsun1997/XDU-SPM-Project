f0aede44652c3b72b8688708fe3c0a990aa00137
Tristone <1272652761@qq.com>
2018-10-06 22:52:43 +0800

    Add Roles Function

--- code/back-end/app.js
+++ code/back-end/app.js
@@ -1,3 +1,15 @@
+
+function randomString(len) {
+	len = len || 32;
+	var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+	var maxPos = $chars.length;
+	var pwd = '';
+	for (i = 0; i < len; i++) {
+		pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+	}
+	return pwd;
+}
+
 //Mongodb package is introduced to support data store.
 var MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017";
@@ -34,7 +46,10 @@ MongoClient.connect(url, { 'useNewUrlParser': true }, function (err, db) {
 				} else {
 					if (doc[0].password == data.password) {
 						successData.type = doc[0].type;
+						successData.session = randomString(32);
 						socket.emit("LoginSuccess", successData);
+						successData.username = doc[0].username;
+						dbase.collection("session").insertOne(successData);
 					} else {
 						socket.emit("wrongPassword");
 					}
@@ -42,10 +57,113 @@ MongoClient.connect(url, { 'useNewUrlParser': true }, function (err, db) {
 			});
 		});
 
+		//if this session exits?
+		socket.on('sessionAuth', function (data) {
+			var auth_cursor = dbase.collection("session").find({ "session": data.session });
+			auth_cursor.toArray(function (err, doc) {
+				test.equal(null, err);
+				if (doc.length == 0) {
+					socket.emit("noSession");
+				} else {
+					if (doc[0].username == data.username && doc[0].type == data.type) {
+						socket.emit("sessionSuccess");
+					} else {
+						socket.emit("sessionFailed");
+					}
+				}
+			});
+		});
 
+		//logout
+		socket.on('logOut', function (data) {
+			dbase.collection("session").deleteOne({ "session": data.session });
+		});
 
+		//about admin
+		socket.on("getAdminInfo", function (data) {
+			var auth_cursor = dbase.collection("session").find({ "session": data.session });
+			auth_cursor.toArray(function (err, doc) {
+				test.equal(null, err);
+				if (doc[0].username == data.username && doc[0].type == data.type) {
+					var admin_cursor = dbase.collection("admin").find({ "username": data.username });
+					admin_cursor.toArray(function (err, doc) {
+						test.equal(null, err);
+						socket.emit("adminInfo", doc[0]);
+					});
+				}
+			});
+		});
 
+		socket.on("updateAdminInfo", function (data) {
+			dbase.collection("admin").updateOne({ "username": data.username }, { $set: { "phone": data.phone, "email": data.email } });
+			socket.emit("updateAdminInfoSuccess");
+		});
 
+		socket.on("updateAdminPassword", function (data) {
+			dbase.collection("accounts").updateOne({ "username": data.username }, { $set: { "password": data.password } });
+			socket.emit("updateAdminPasswordSuccess");
+		});
+
+		//about librarian
+		socket.on("getLibrarianInfo", function (data) {
+			var auth_cursor = dbase.collection("session").find({ "session": data.session });
+			auth_cursor.toArray(function (err, doc) {
+				test.equal(null, err);
+				if (doc[0].username == data.username && doc[0].type == data.type) {
+					var cursor = dbase.collection("librarian").find({ "username": data.username });
+					cursor.toArray(function (err, doc) {
+						test.equal(null, err);
+						socket.emit("librarianInfo", doc[0]);
+					});
+				}
+			});
+		});
+
+		socket.on("updateLibrarianInfo", function (data) {
+			dbase.collection("librarian").updateOne({ "username": data.username }, { $set: { "phone": data.phone, "email": data.email } });
+			socket.emit("updateLibrarianInfoSuccess");
+		});
+
+		socket.on("updateLibrarianPassword", function (data) {
+			dbase.collection("accounts").updateOne({ "username": data.username }, { $set: { "password": data.password } });
+			socket.emit("updateLibrarianPasswordSuccess");
+		});
+
+		socket.on("addLibrarianRole", function (data) {
+			dbase.collection("librarian").insertOne(data, function (err, res) {
+				dbase.collection("librarian").updateOne({ "_id": res.insertedId }, { $set: { "username": res.insertedId } });
+				socket.emit("addLibrarianRoleSuccess");
+				var auth_data = {};
+				auth_data.username = res.insertedId;
+				auth_data.password = "123456";
+				auth_data.type = "librarian";
+				dbase.collection("accounts").insertOne(auth_data);
+			});
+		});
+
+		socket.on("addReaderRole", function (data) {
+			dbase.collection("reader").insertOne(data, function (err, res) {
+				dbase.collection("reader").updateOne({ "_id": res.insertedId }, { $set: { "username": res.insertedId } });
+				socket.emit("addReaderRoleSuccess");
+				var auth_data = {};
+				auth_data.username = res.insertedId;
+				auth_data.password = "123456";
+				auth_data.type = "reader";
+				dbase.collection("accounts").insertOne(auth_data);
+			});
+		});
+		/*
+				socket.on("editLibrarianRole",function(data){
+					dbase.collection("librarian").updateOne(data, function(res){
+						socket.emit("addLibrarianRoleSuccess");
+						var auth_data = {};
+						auth_data.username = res.insertedId;
+						auth_data.password = "123456";
+						auth_data.type = "librarian";
+						dbase.collection("accounts").insertOne(auth_data);
+					});
+				}); 
+		*/
 
 		//The scope which all bussiness defined in. end--------------------------------------------------------------
 	});
